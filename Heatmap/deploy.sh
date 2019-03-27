5e09862ca76fa4a34dce7c9e00a7d8d2b2a393fe
Detchat Boonpragob <Oudy1st@pc-73-85.customer.stratford.derwent.lan>
2019-03-27 19:42:42 +0000

    Update

---
+++ Heatmap/deploy.sh
@@ -0,0 +1,15 @@
+export PROJECT_ID="$(gcloud config get-value project -q)"
+gcloud config set project $PROJECT_ID
+gcloud config set compute/zone us-central1-b
+
+docker build -t gcr.io/${PROJECT_ID}/heatmap:v1 .
+gcloud auth configure-docker
+docker push gcr.io/${PROJECT_ID}/heatmap:v1
+
+gcloud container clusters create heatmap-cluster --num-nodes=3
+
+kubectl run web-app-server --image=gcr.io/${PROJECT_ID}/heatmap:v1 --port 8080
+
+kubectl expose deployment web-app-server --type=LoadBalancer --port 80 --target-port 8080
+
+kubectl scale deployment web-app-server --replicas=4
